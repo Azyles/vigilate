c30b504c3eaf81779a74938fdb12c358afd9f279
Kushagra Singh <kushthaksh@gmail.com>
2020-10-10 16:20:56 -0700

    Police View

---
+++ lib/police.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PoliceView extends StatefulWidget {
+  @override
+  _PoliceViewState createState() => _PoliceViewState();
+}
+
+class _PoliceViewState extends State<PoliceView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
