4c928e86c1bb82631db668e07161ace3fe4b50fd
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2020-10-10 13:32:46 -0700

    Title for view

--- lib/view.dart
+++ lib/view.dart
@@ -11,7 +11,32 @@ class ReportListViewState extends State<ReportListView> {
     return Scaffold(
       body: ListView(
         children: [
+
+          //Title Text and Icon ////
+          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+          Center(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'West Valley Mall',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w300),
+              ),
+              Container(
+                width: 30,
+              ),
+              Icon(Icons.message_outlined)
+            ],
+          )),
+
+          //End of Title Text and Icon ///
+
           
+
+
         ],
       ),
     );
