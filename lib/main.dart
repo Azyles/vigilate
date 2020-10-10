46737d9ec0a2b9e8b3503236a76c07adc9a59001
Pranav Krishna <krishnatechpranav@gmail.com>
2020-10-10 11:07:43 -0700

    adsf

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:vigilate/backend.dart';
 import 'package:vigilate/report.dart';
 import 'package:vigilate/view.dart';
-import '';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
