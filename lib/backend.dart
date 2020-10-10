316947859ab46374779d7815221c6bd04389c48d
Pranav Krishna <krishnatechpranav@gmail.com>
2020-10-10 10:15:46 -0700

    asdf

--- lib/backend.dart
+++ lib/backend.dart
@@ -1,10 +1,37 @@
+import 'package:flutter/services.dart';
+import 'package:geocoder/geocoder.dart';
 import 'package:location/location.dart';
 
 class Backend {
   
-  getCurrentLocation() {
+  getCurrentLocation() async {
     LocationData currentLocation;
 
-    
+    LocationData myLocation;
+      String error;
+      Location location = new Location();
+      try {
+        myLocation = await location.getLocation();
+      } on PlatformException catch (e) {
+        if (e.code == 'PERMISSION_DENIED') {
+          error = 'please grant permission';
+          print(error);
+        }
+        if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
+          error = 'permission denied- please enable it from app settings';
+          print(error);
+        }
+        myLocation = null;
+      }
+      currentLocation = myLocation;
+      final coordinates = new Coordinates(
+          myLocation.latitude, myLocation.longitude);
+      var addresses = await Geocoder.local.findAddressesFromCoordinates(
+          coordinates);
+      var first = addresses.first;
+      print(' ${first.locality}, ${first.adminArea},${first.subLocality}, ${first.subAdminArea},${first.addressLine}, ${first.featureName},${first.thoroughfare}, ${first.subThoroughfare}');
+      return first;
+
+
   }
 }
