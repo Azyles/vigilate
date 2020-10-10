a5e6b5d03c07ec602fca59d295a3f77fe01f3f13
Pranav Krishna <krishnatechpranav@gmail.com>
2020-10-10 10:27:05 -0700

    asdf

--- lib/backend.dart
+++ lib/backend.dart
@@ -1,43 +1,22 @@
 import 'package:flutter/services.dart';
 import 'package:geocoder/geocoder.dart';
-import 'package:location/location.dart';
 import 'package:geolocator/geolocator.dart';
 
 class Backend {
-  
+  var city = '';
+
   getCurrentLocation() async {
-    LocationData currentLocation;
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print('location: ${position.latitude}');
+    final coordinates = new Coordinates(position.latitude, position.longitude);
+    var addresses =
+        await Geocoder.local.findAddressesFromCoordinates(coordinates);
+    var first = addresses.first;
+    print("${first.featureName} : ${first.addressLine}");
 
-    LocationData myLocation;
+    print("City : ${first.locality}");
 
-      String error;
-      Location location = new Location();
-      try {
-        myLocation = await location.getLocation();
-      } on PlatformException catch (e) {
-        if (e.code == 'PERMISSION_DENIED') {
-          error = 'please grant permission';
-          print(error);
-        }
-        if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
-          error = 'permission denied- please enable it from app settings';
-          print(error);
-        }
-        myLocation = null;
-      }
-      currentLocation = myLocation;
-
-
-      final coordinates = new Coordinates(
-          myLocation.latitude, myLocation.longitude);
-
-      var addresses = await Geocoder.local.findAddressesFromCoordinates(
-          coordinates);
-
-      var first = addresses.first;
-      print(' ${first.locality}, ${first.adminArea},${first.subLocality}, ${first.subAdminArea},${first.addressLine}, ${first.featureName},${first.thoroughfare}, ${first.subThoroughfare}');
-
-
-
+    city = first.locality;
   }
 }
