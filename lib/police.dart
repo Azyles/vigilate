6466b3d82b37735d85e0f1bc62e8518ebe12dc52
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2020-10-10 17:23:19 -0700

    Alert Page Finsihed

--- lib/police.dart
+++ lib/police.dart
@@ -54,6 +54,10 @@ class _PoliceViewState extends State<PoliceView> {
                 ),
                ),
 
+               Container(
+                  height: 10,
+                ),
+
                Row(
                  children: [
                    Switch(
@@ -67,9 +71,61 @@ class _PoliceViewState extends State<PoliceView> {
                       activeTrackColor: Colors.lightGreenAccent,
                       activeColor: Colors.green,
                     ),
+                    Container(
+                      width: 40,
+                    ),
+
+                    Text(
+                      'Alert Sound',
+                      style: TextStyle(
+                        fontSize: 20
+
+                      ),
+                    )
+                 ]
                   ),
-                 ],
-               )
+
+                  
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[900], width: 2)),
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+                  child: TextField(
+                    maxLines: 10,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Short and clear description of the event.',
+                      hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              height: 20,
+            ),
+
+            Center(
+              child: Container(
+                width: 300,
+                child: Text(
+                  'Info: Keep in mind before you alert everyone at your location.  Once you alert everyone, each person will get a notification.  Only use Alert Sound when situation is dire'
+                )
+              ),
+            ),
+
+            
+                 
+               
               
                 ],
               )
