c1a9c7425bddbaeb3cc6fe2456155a7d69092e78
Pranav Krishna <krishnatechpranav@gmail.com>
2020-10-10 18:48:19 -0700

    asdf

--- lib/police.dart
+++ lib/police.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:vigilate/backend.dart';
 
 class PoliceView extends StatefulWidget {
   @override
@@ -7,6 +9,29 @@ class PoliceView extends StatefulWidget {
 
 class _PoliceViewState extends State<PoliceView> {
   bool isSwitched = false;
+
+  var descriptionController = new TextEditingController();
+
+  alertNearby() async {
+    var firestore = FirebaseFirestore.instance;
+
+    var backendInstance = new Backend();
+
+    var location = backendInstance.getCurrentLocation();
+
+    firestore
+        .collection("Cities")
+        .doc(location[2])
+        .collection("Alerts")
+        .doc()
+        .set({
+      "longitude": location[0],
+      "latitude": location[1],
+      "description": descriptionController.text,
+      "time": new DateTime.now()
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,55 +41,45 @@ class _PoliceViewState extends State<PoliceView> {
               child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: 80
-              ),
+              Container(height: 80),
               Column(
                 children: [
                   Text(
-                'Alert',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w300),
-              ),
-              Container(
-                height: 50
-              ),
-               GestureDetector(
-                 child: Container(
-                  height: 300,
-                  width: 300,
-                   decoration:
-                     BoxDecoration(color: Colors.red[300],
-                    borderRadius: BorderRadius.circular(300)
-                    ),
-
-                    child: Center(
-                      child: Text(
-                        'ALERT CURRENT LOCATION',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.w700,
+                    'Alert',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  Container(height: 50),
+                  GestureDetector(
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: Colors.red[300],
+                          borderRadius: BorderRadius.circular(300)),
+                      child: Center(
+                        child: Text(
+                          'ALERT CURRENT LOCATION',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
                     ),
-
-                ),
-               ),
-
-               Container(
-                  height: 10,
-                ),
-
-               Row(
-                 children: [
-                   Switch(
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Row(children: [
+                    Switch(
                       value: isSwitched,
-                      onChanged: (value){
+                      onChanged: (value) {
                         setState(() {
-                          isSwitched=value;
+                          isSwitched = value;
                           print(isSwitched);
                         });
                       },
@@ -74,59 +89,46 @@ class _PoliceViewState extends State<PoliceView> {
                     Container(
                       width: 40,
                     ),
-
                     Text(
                       'Alert Sound',
-                      style: TextStyle(
-                        fontSize: 20
-
-                      ),
+                      style: TextStyle(fontSize: 20),
                     )
-                 ]
-                  ),
-
-                  
-            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey[900], width: 2)),
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-                  child: TextField(
-                    maxLines: 10,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Short and clear description of the event.',
-                      hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500),
+                  ]),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(color: Colors.grey[900], width: 2)),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 5),
+                        child: TextField(
+                          controller: descriptionController,
+                          maxLines: 10,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText:
+                                'Short and clear description of the event.',
+                            hintStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
-
-            Container(
-              height: 20,
-            ),
-
-            Center(
-              child: Container(
-                width: 300,
-                child: Text(
-                  'Info: Keep in mind before you alert everyone at your location.  Once you alert everyone, each person will get a notification.  Only use Alert Sound when situation is dire'
-                )
-              ),
-            ),
-
-            
-                 
-               
-              
+                  Container(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Container(
+                        width: 300,
+                        child: Text(
+                            'Info: Keep in mind before you alert everyone at your location.  Once you alert everyone, each person will get a notification.  Only use Alert Sound when situation is dire')),
+                  ),
                 ],
               )
             ],
